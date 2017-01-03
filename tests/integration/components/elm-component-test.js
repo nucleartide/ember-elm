2ef3cc541dfe59373c06ed2b9292baaa60c6cf2a
Jason Tu <jasontu4@gmail.com>
2017-01-03 03:59:39 -0500

    Add test

---
+++ tests/integration/components/elm-component-test.js
@@ -0,0 +1,19 @@
+
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
+import Elm from 'dummy/tests/elm-modules'
+
+moduleForComponent('elm-component', 'Integration | Component | elm component', {
+  integration: true
+})
+
+test('it works', function(assert) {
+  const done = assert.async()
+  this.set('Elm', Elm)
+  this.render(hbs`{{elm-component src=Elm.Hello}}`)
+
+  Ember.run.next(() => {
+    assert.equal(this.$().text().trim(), 'hello world')
+    done()
+  })
+})
