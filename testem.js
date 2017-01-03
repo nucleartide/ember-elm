de6d804fed1a6e791a4e820e39719e1df8cbb94b
Jason Tu <jasontu4@gmail.com>
2017-01-03 04:29:24 -0500

    Add testem.js so ember test works

---
+++ testem.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "Chrome"
+  ],
+  "launch_in_dev": [
+    "Firefox",
+    "Chrome",
+    "Safari",
+    "Opera"
+  ]
+};
